dbb9a22ddf2a8a9284258c25df3c45a555e5818d
xsyu <yrxxsyu@163.com>
2013-02-18 10:45:00 +0800

    Initial Commit

---
+++ D/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  D
+//
+//  Created by Ibokan on 13-2-18.
+//  Copyright (c) 2013年 ios43. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ D/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  D
+//
+//  Created by Ibokan on 13-2-18.
+//  Copyright (c) 2013年 ios43. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
